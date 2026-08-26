d2a868d911b84436a4e98c763b764233e4d6454a
chinctor <chinctor@gmail.com>
2026-08-26 09:21:25 +0700

    Declare modules: png, avif, webp

--- foundations/utils/packages/platform-rig/profiles/ui/svelte/index.d.ts
+++ foundations/utils/packages/platform-rig/profiles/ui/svelte/index.d.ts
@@ -2,3 +2,17 @@ declare module "*.svelte" {
   const value: any;
   export default value;
 }
+declare module '*.png' {
+  const url: string
+  export default url
+}
+
+declare module '*.avif' {
+  const url: string
+  export default url
+}
+
+declare module '*.webp' {
+  const url: string
+  export default url
+}
